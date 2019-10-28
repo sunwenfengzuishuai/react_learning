8ca3fcbbc09a7544b99cdf158f014e51e1dd08dc
孙文丰 <1464559642@qq.com>
2019-10-28 15:10:48 +0800

    react-app

---
+++ hello-react/src/components/app.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+import './app.css'
+
+export default class App extends Component{
+    render(){
+        return (
+            <div className="hi">hello,React</div>
+        )
+    }
+}
